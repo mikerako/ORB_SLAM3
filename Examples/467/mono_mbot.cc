b5519dcb19b6907a11fa1baf98f60100bd019f31
Aakash Venkatesan <venkatesan.aakash@gmail.com>
2020-10-16 22:13:35 -0400

    added file to run one sequence of visual slam with prerecorded data

---
+++ Examples/467/mono_mbot.cc
@@ -0,0 +1,170 @@
+/**
+* This file is part of ORB-SLAM3
+*
+* Copyright (C) 2017-2020 Carlos Campos, Richard Elvira, Juan J. Gómez Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+* Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+*
+* ORB-SLAM3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+* License as published by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+* the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with ORB-SLAM3.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<chrono>
+
+#include<opencv2/core/core.hpp>
+
+#include<System.h>
+
+using namespace std;
+
+void LoadImages(const string &strImagePath, const string &strPathTimes,
+                vector<string> &vstrImages, vector<double> &vTimeStamps);
+
+int main(int argc, char **argv)
+{  
+    if(argc < 5)
+    {
+        cerr << endl << "Usage: ./mono_mbot path_to_vocabulary path_to_settings path_to_image_folder path_to_times_file (trajectory_file_name)" << endl;
+        return 1;
+    }
+
+    const int num_seq = (argc-3)/2;
+    cout << "num_seq = " << num_seq << endl;
+    bool bFileName= (((argc-3) % 2) == 1);
+    string file_name;
+    if (bFileName)
+    {
+        file_name = string(argv[argc-1]);
+        cout << "file name: " << file_name << endl;
+    }
+
+    // Only using one image sequence:
+    vector<string> vstrImageFilenames;
+    vector<double> vTimestampsCam;
+    int nImages;
+
+    int tot_images = 0;
+
+    cout << "Loading images" << "...";
+    // argv[3] holds absolute path to images directory i.e. ~/Datasets/EuRoC/MH01/mav0/cam0/data
+    // Example for our 467 structure: ~/Datasets/vSLAM/images
+    // arg[4] holds absolute path to timestamp file i.e ~/Examples/Monocular/EuRoC_TimeStamps/MH01.txt
+    // Example for our 467 structure ~/Datasets/vSLAM/timestamps.txt
+
+    LoadImages(string(argv[3]), string(argv[4]), vstrImageFilenames, vTimestampsCam);
+    cout << "LOADED!" << endl;
+
+    tot_images = nImages = vstrImageFilenames.size();
+
+    // Vector for tracking time statistics
+    vector<float> vTimesTrack;
+    vTimesTrack.resize(tot_images);
+
+    cout << endl << "-------" << endl;
+    cout.precision(17);
+
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM3::System SLAM(argv[1],argv[2],ORB_SLAM3::System::MONOCULAR, true);
+
+    // Main loop
+    cv::Mat im;
+    int proccIm = 0;
+    for(int ni=0; ni<nImages; ni++, proccIm++)
+    {
+
+        // Read image from file
+        im = cv::imread(vstrImageFilenames[ni],CV_LOAD_IMAGE_UNCHANGED);
+        double tframe = vTimestampsCam[ni];
+
+        if(im.empty())
+        {
+            cerr << endl << "Failed to load image at: "
+                <<  vstrImageFilenames[ni] << endl;
+            return 1;
+        }
+
+#ifdef COMPILEDWITHC11
+        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+#else
+        std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
+#endif
+
+        // Pass the image to the SLAM system
+        // cout << "tframe = " << tframe << endl;
+        SLAM.TrackMonocular(im,tframe); // TODO change to monocular_inertial
+
+#ifdef COMPILEDWITHC11
+        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+#else
+        std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
+#endif
+        double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
+        vTimesTrack[ni]=ttrack;
+        // Wait to load the next frame
+
+        double T=0;
+        if(ni<nImages-1)
+            T = vTimestampsCam[ni+1]-tframe;
+
+        else if(ni>0)
+            T = tframe-vTimestampsCam[ni-1];
+
+        if(ttrack<T)
+            usleep((T-ttrack)*1e6); // 1e6
+    }
+
+    // Stop all threads
+    SLAM.Shutdown();
+
+    // Save camera trajectory
+    if (bFileName)
+    {
+        const string kf_file =  "kf_" + string(argv[argc-1]) + ".txt";
+        const string f_file =  "f_" + string(argv[argc-1]) + ".txt";
+        SLAM.SaveTrajectoryEuRoC(f_file);
+        SLAM.SaveKeyFrameTrajectoryEuRoC(kf_file);
+    }
+    else
+    {
+        SLAM.SaveTrajectoryEuRoC("CameraTrajectory.txt");
+        SLAM.SaveKeyFrameTrajectoryEuRoC("KeyFrameTrajectory.txt");
+    }
+
+    return 0;
+}
+
+void LoadImages(const string &strImagePath, const string &strPathTimes,
+                vector<string> &vstrImages, vector<double> &vTimeStamps)
+{
+    ifstream fTimes;
+    fTimes.open(strPathTimes.c_str());
+    vTimeStamps.reserve(5000);
+    vstrImages.reserve(5000);
+    
+    while(!fTimes.eof())
+    {
+        string s;
+        getline(fTimes,s);
+        if(!s.empty())
+        {
+            stringstream ss;
+            ss << s;
+            vstrImages.push_back(strImagePath + "/" + ss.str() + ".png");
+            double t;
+            ss >> t;
+            vTimeStamps.push_back(t/1e9);
+        }
+    }
+}
